d5abcecf15241b0a779bb085aa082b85dadd18ea
dzakyzf <dzakybd@gmail.com>
2019-03-06 20:00:04 +0900

    commit

--- ServerMaktab/app.py
+++ ServerMaktab/app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import json
@@ -7,17 +7,10 @@ import json
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SECRET_KEY'] = "random string"
+app.config['SECRET_KEY'] = "dzaky"
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
-class UserSchema(ma.Schema):
-    class Meta:
-        # Fields to expose
-        fields = ('akunid', 'nama', 'nohp')
-
-user_schema = UserSchema()
-users_schema = UserSchema(many=True)
 
 
 class Akun(db.Model):
@@ -116,16 +109,146 @@ class Rombongan(db.Model):
     akun = db.relationship(u'Akun', primaryjoin='Rombongan.koordinator == Akun.akunid', backref=u'rombongans')
 
 
+param_akunid = "akunid"
+param_nama = "nama"
+param_nohp = "nohp"
+param_peran = "peran"
+param_daerahpdkr = "daerahpdkr"
+param_rombongankrjamaah = "rombongankrjamaah"
+param_aktif = "aktif"
+param_lat = "lat"
+param_lng = "lng"
+param_keteranganpengurus = "keteranganpengurus"
+param_daerahid = "daerahid"
+param_namadaerah = "namadaerah"
+param_judul = "judul"
+param_nilai = "nilai"
+param_waktu = "waktu"
+param_jadwalmakanid = "jadwalmakanid"
+param_maktabid = "maktabid"
+param_namapemilik = "namapemilik"
+param_nohppemilik = "nohppemilik"
+param_alamat = "alamat"
+param_kategori = "kategori"
+param_kapasitas = "kapasitas"
+param_riwayatdaerah = "riwayatdaerah"
+param_parkirid = "parkirid"
+param_status = "status"
+param_rombonganid = "rombonganid"
+param_namarombongan = "namarombongan"
+param_koordinator = "koordinator"
+param_namapo = "namapo"
+param_jeniskendaraan = "jeniskendaraan"
+param_jumlahjamaah = "jumlahjamaah"
+param_waktuberangkat = "waktuberangkat"
+param_waktutiba = "waktutiba"
+param_alokasimaktab = "alokasimaktab"
+param_alokasiparkir = "alokasiparkir"
+    
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('akunid', 'nama', 'nohp')
 
 
 @app.route('/')
 def hello_world():
+
+    users_schema = UserSchema(many=True)
+
     all_users = Akun.query.all()
     result = users_schema.dump(all_users)
     return jsonify(result.data)
 
 
+@app.route('/login', methods=['POST'])
+def login():
+    return "result"
+
+
+@app.route('/data/akun', methods=['POST'])
+def data_akun():
+    return "result"
+
+
+@app.route('/data/jadwalmakan', methods=['POST'])
+def data_jadwalmakan():
+    return "result"
+
+
+@app.route('/data/maktab', methods=['POST'])
+def data_maktab():
+    return "result"
+
+
+@app.route('/data/parkir', methods=['POST'])
+def data_parkir():
+    return "result"
+
+
+@app.route('/data/rombongan', methods=['POST'])
+def data_rombongan():
+    return "result"
+
+
+@app.route('/data/daerah', methods=['POST'])
+def data_daerah():
+    return "result"
+
+
+@app.route('/data/pengambilanmakanan', methods=['POST'])
+def data_pengambilanmakanan():
+    return "result"
+
+
+@app.route('/list/pengambilanmakanan', methods=['POST'])
+def list_pengambilanmakanan():
+    return "result"
+
+
+@app.route('/list/penghunimaktab', methods=['POST'])
+def list_penghunimaktab():
+    return "result"
+
+
+@app.route('/list/penghuniparkir', methods=['POST'])
+def list_penghuniparkir():
+    return "result"
+
+
+@app.route('/list/daerah', methods=['POST'])
+def list_daerah():
+    return "result"
+
+
+@app.route('/menu/akun', methods=['POST'])
+def menu_akun():
+    return "result"
+
+
+@app.route('/menu/dashboard', methods=['POST'])
+def menu_dashboard():
+    return "result"
+
+
+@app.route('/menu/jadwalmakan', methods=['POST'])
+def menu_jadwalmakan():
+    return "result"
+
+
+@app.route('/menu/maktab', methods=['POST'])
+def menu_maktab():
+    return "result"
+
+
+@app.route('/menu/rombongan', methods=['POST'])
+def menu_rombongan():
+    return "result"
+
+
+@app.route('/menu/parkir', methods=['POST'])
+def menu_parkir():
+    return "result"
+
 
 if __name__ == '__main__':
-    db.create_all()
     app.run(debug=True, host='0.0.0.0', threaded=True)
